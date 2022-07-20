ac7c84696cd6a0c1186ead943834134339cfdb73
unknown <emilianomartindominguezfrese@gmail.com>
2022-07-20 20:23:57 -0300

    started style / finished default input

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -11,98 +11,25 @@ const Input = ({
   endIcon,
   size,
   color,
-  iconName
+  iconName,
 }) => {
   let classInput = classes.inputDefault;
   let style = {};
-  let title = '<Input '
- 
-  if (typeof variant !== "undefined" && variant !== "") {
-    title += `variant="${variant}" `;
-    switch (variant) {
-      case "outline":
-        classInput = classes.outlineInput;
-        break;
-      case "text":
-        classInput = classes.textInput;
-        break;
-    }
-  }
+  let title = "<Input ";
 
-  if (disabledShadow) {
-    title += 'disabledShadow '
-    style.boxShadow = "none";
-  }
-
-  if (disabled) {
-    title += 'disabled '
-    style.color = "#9e9e9e";
-    style.pointerEvents = 'none'
-  }
-
-  if (startIcon) {
-    title += `startIcon=${iconName} `
-    style.paddingRight = "16px";
-  }
-
-  if (endIcon) {
-    title += `endIcon=${iconName} `
-    style.paddingLeft = "16px";
-  }
-
-  if (typeof size !== "undefined" && size !== ""){
-    title += `size="${size}" `
-    switch (size) {
-      case "sm":
-        style.width = "73px";
-        style.height = "32px";
-        style.minWidth = "73px";
-        style.minHeight = "32px";
-        break;
-      case "md":
-        style.width = "81px";
-        style.height = "36px";
-        break;
-      case "lg":
-        style.width = "93px";
-        style.height = "42px";
-    }
-  }
-
-  if (typeof color !== "undefined" && color !== ""){
-    title += `color="${color}" `
-    switch (color) {
-      case "primary":
-        classInput = classes.primary;
-        break;
-      case "secondary":
-        classInput = classes.secondary;
-        break;
-      case "danger":
-        classInput = classes.danger;
-        break;
-    }
-  }
-
-
-  title += '/>'
+  title += "/>";
 
   return (
     <div className={classes.inputContainer}>
       <p className={classes.inputTitle}>{title}</p>
-      <button
-        className={classInput}
-        style={style}
-        onClick={() => {
-          if (!disabled) {
-            toggle();
-          }
-        }}
-      >
-        {startIcon && <img src={startIcon} className={classes.startImg} />}
-        Default
-        {endIcon && <img src={endIcon} className={classes.endImg} />}
-      </button>
+      <label className={classes.inputLabel}>
+        <p>Label</p>
+        <input
+          className={classInput}
+          style={style}
+          placeholder={"Placeholder"}
+        />
+      </label>
     </div>
   );
 };
--- src/pages/InputPage.jsx
+++ src/pages/InputPage.jsx
@@ -11,18 +11,6 @@ const InputPage = () => {
         <SideBar />
         <InputsWrappers>
           <Input />
-          <Input variant="outline" />
-          <Input variant="text" />
-          <Input disabledShadow />
-          <Input disabled />
-          <Input variant="text" disabled />
-          <Input size="sm" />
-          <Input size="md" />
-          <Input size="lg" />
-          <Input color="default" />
-          <Input color="primary" />
-          <Input color="secondary" />
-          <Input color="danger" />
         </InputsWrappers>
       </InputPageWrapper>
   );
