34fa8de00b57ca1d8a66c3eefb8891681b797989
unknown <emilianomartindominguezfrese@gmail.com>
2022-07-21 10:56:40 -0300

    started icon inputs

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -2,33 +2,77 @@ import React from "react";
 
 import classes from "./Input.module.css";
 
-const Input = ({
-  variant,
-  disabledShadow,
-  disabled,
-  toggle,
-  startIcon,
-  endIcon,
-  size,
-  color,
-  iconName,
-}) => {
+const Input = ({ error, disabled, helperText, startIcon, endIcon, iconName }) => {
   let classInput = classes.inputDefault;
+  let classLabel = classes.labelDefault;
+  let classHelper = classes.helperDefault;
   let style = {};
   let title = "<Input ";
+
+  if(typeof helperText !== "undefined" && helperText !== ""){
+    title += `helperText="${helperText}" `;
+  }
+
+  if(startIcon) {
+    title += `startIcon={${iconName}}`
+    style.background =  `url(${startIcon}) no-repeat`
+    style.backgroundSize = '18px';
+    style.backgroundPosition = '5%'
+  }
+
+  if(endIcon) {
+    title += `endIcon={${iconName}}`
+  }
+
+  if (error) {
+    title += "error ";
+    classInput = classes.inputError;
+    classLabel = classes.labelError;
+  }
+
+  if(disabled){
+    title += "disabled "
+    style.pointerEvents = 'none';
+    style.outline = '1px solid #E0E0E0'
+  }
+
 
   title += "/>";
 
   return (
     <div className={classes.inputContainer}>
       <p className={classes.inputTitle}>{title}</p>
-      <label className={classes.inputLabel}>
-        <p>Label</p>
+      <label className={classLabel}>
+        <p
+          className={classes.labelP}
+          style={{
+            textAlign: "left",
+            fontFamily: "Noto Sans JP",
+            fontSize: "12px",
+            fontWeight: 400,
+            margin: 0,
+          }}
+        >
+          Label
+        </p>
         <input
           className={classInput}
           style={style}
           placeholder={"Placeholder"}
+          disabled={disabled}
         />
+        {typeof helperText !== "undefined" && helperText !== "" && 
+          <p
+          style={{
+            textAlign: "left",
+            fontFamily: "Noto Sans JP",
+            fontSize: "12px",
+            fontWeight: 400,
+            margin: 0,
+          }}
+          className={classes.underLabelP}
+          >{helperText}</p>
+        }
       </label>
     </div>
   );
--- src/pages/InputPage.jsx
+++ src/pages/InputPage.jsx
@@ -5,12 +5,24 @@ import InputsWrappers from "../components/Input/InputsWrappers";
 import SideBar from "../components/SideBar/SideBar";
 import InputPageWrapper from "../components/UI/InputPageWrapper";
 
+import workImg from '../assets/work_FILL0_wght400_GRAD0_opsz48.svg';
+import mailImg from '../assets/mail_FILL0_wght400_GRAD0_opsz48.svg';
+import personImg from '../assets/person_FILL0_wght400_GRAD0_opsz48.svg';
+import callImg from '../assets/call_FILL0_wght400_GRAD0_opsz48.svg';
+import musicImg from '../assets/music_note_FILL0_wght400_GRAD0_opsz48.svg';
+
 const InputPage = () => {
   return (
       <InputPageWrapper>
         <SideBar />
         <InputsWrappers>
           <Input />
+          <Input error />
+          <Input disabled />
+          <Input helperText={"Some interesting text"} />
+          <Input helperText={"Some interesting text"} error />
+          <Input startIcon={workImg} iconName={"workImg"} />
+          <Input endIcon={callImg} iconName={"callImg"} />
         </InputsWrappers>
       </InputPageWrapper>
   );
